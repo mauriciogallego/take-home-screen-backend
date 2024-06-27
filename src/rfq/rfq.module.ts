d221239f90e82808fe57ceec8a735c2614993979
mauricio gallego <mgallego@sysgarage.com>
2024-06-26 21:11:59 -0500

    fix:messing connection

--- src/rfq/rfq.module.ts
+++ src/rfq/rfq.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RfqController } from './rfq.controller';
 import { RfqService } from './rfq.service';
+import { ChatgptService } from './chatgpt.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [RfqController],
-  providers: [RfqService],
+  providers: [RfqService, ChatgptService],
 })
 export class RfqModule {}
