406aba77a49fed50a886c3c447eecbf207113bbc
mauricio gallego <mgallego@sysgarage.com>
2024-06-30 20:04:29 -0500

    feature: saving address

--- src/constants/rfq.constant.ts
+++ src/constants/rfq.constant.ts
@@ -2,4 +2,4 @@ export const isRfq =
   'Answer the next question with YES or NO. is the following text a RFQ?\n';
 
 export const getProductList = (productNames: string, email: string) =>
-  `Give me a JSON of the products from the following text, specifying name, unit, dimensions, quantity, and expiration date. If there is no information, then set it to null. If there are products named similarly to ${productNames} and change them:\n ${email}`;
+  `Give me a JSON of the products from the following text, specifying name, unit, dimensions, quantity, and expiration date and give me the address as well. If there is no information, then set it to null. If there are products named similarly to ${productNames} and change them:\n ${email}`;
